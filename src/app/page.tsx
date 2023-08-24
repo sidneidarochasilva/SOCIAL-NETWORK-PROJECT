2b71379d867bd077870afdf14b563a2410d47e8e
Sidnei da Rocha Silva <sidneidrs@gmail.com>
2023-08-24 00:22:00 -0300

    - inserindo funcionalidade ao campo de busca - implementando pagina de detalhes do post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,27 +6,41 @@ import React, { useEffect, useState } from 'react';
 import { getPosts } from '@/dummy_api/dummy_api';
 import ListPost from './components/ListPost';
 
+
+
+
 export default function Home() {
- 
 
   const [postagens, setPostagens] = useState([]);
+  const [tempList, setTempList] = useState([])
 
-   useEffect(() => {
+  useEffect(() => {
 
     getPosts().then((response: any) => {
-      setPostagens(response.data)
-      console.log("response", response.data)
+      setPostagens(response.data.data)
+      setTempList(response.data.data)
     })
-
-  },[]); 
-
+  }, []);
 
 
+
+  const handleFilteredList = (filterList: any) => {
+
+    if (filterList.length === 0) {
+      setPostagens(tempList);
+      return
+    }
+    setPostagens(filterList);
+
+  };
 
   return (
-    <>
-      <Header />
-      <ListPost posts={postagens} />
-    </>
+    <div className='p-3'>
+      <Header posts={postagens} onFiltered={handleFilteredList} />
+      <div className='mt-[120px]'>
+        <ListPost posts={postagens} />
+      </div>
+
+    </div>
   )
 }
